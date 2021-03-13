5ffc0100027c7ef174fbd45e921a32448018f01f
eber404 <bodacamargo@gmail.com>
2021-03-13 13:49:43 -0300

    first commit

---
+++ src/layouts/Main.js
@@ -0,0 +1,11 @@
+import { Box, Flex } from 'reflexbox'
+
+const Main = () => {
+  return (
+    <Flex>
+      <Box height="100%" width="100%" maxWidth="1280px" marginX="35px"></Box>
+    </Flex>
+  )
+}
+
+export default Main
